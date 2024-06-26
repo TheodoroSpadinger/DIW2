e92c1ecc5522a44d80a5f50dc3d056865f8d33a7
Theodoro Augusto <theodorobsm@gmail.com>
2024-06-26 19:14:52 -0300

    Trabalho Pronto

--- githubapi.js
+++ githubapi.js
@@ -1,18 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const username = 'TheodoroSpadinger'; // Define o nome de usuário do GitHub
-  const token = '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'; // Substitua pelo seu token pessoal
-  const url = `https://api.github.com/users/${username}/repos`;
+  const apiUrl = 'http://localhost:3000'; 
 
-  fetch(url, {
+  
+  function carregarPerfil() {
+    fetch(`${apiUrl}/perfil`)
+      .then(response => response.json())
+      .then(data => {
+        // Exibir nome e descrição do perfil
+        const perfilHeader = document.getElementById('perfilHeader');
+        perfilHeader.innerHTML = `
+          <h2>${data.nome}</h2>
+          <p class="ms-6">${data.descricao}</p>
+          <p class="localizacao"><b>Localização:</b> ${data.localizacao}</p>
+        `;
+      })
+      .catch(error => console.error('Erro ao buscar perfil:', error));
+  }
+
+ 
+  carregarPerfil();
+
+  
+  const username = 'TheodoroSpadinger';
+  const githubInfoUrl = `https://api.github.com/users/${username}`;
+
+  
+  fetch(githubInfoUrl)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('github-repos').innerText = `Repositórios públicos: ${data.public_repos}`;
+      document.getElementById('github-followers').innerText = `Seguidores: ${data.followers}`;
+    })
+    .catch(error => {
+      console.error('Erro ao buscar informações do GitHub:', error);
+      document.getElementById('github-repos').innerText = 'Erro ao buscar repositórios';
+      document.getElementById('github-followers').innerText = 'Erro ao buscar seguidores';
+    });
+
+ 
+  const token = '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'; 
+  const reposUrl = `https://api.github.com/users/${username}/repos`;
+
+  
+  fetch(reposUrl, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data); // Verifique os dados recebidos no console
-
-      // Manipule os dados como necessário e atualize o DOM
       const reposContainer = document.getElementById('reposContainer');
       if (reposContainer) {
         data.forEach(repo => {
@@ -40,5 +76,81 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(error => {
       console.error('Erro ao buscar repositórios:', error);
+    });
+
+  
+  const conteudosSugeridosUrl = `${apiUrl}/conteudosSugeridos`; 
+
+  fetch(conteudosSugeridosUrl)
+    .then(response => response.json())
+    .then(data => {
+      const carouselInner = document.querySelector('#carouselExampleControls .carousel-inner');
+
+      data.forEach((conteudo, index) => {
+        const carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+        if (index === 0) {
+          carouselItem.classList.add('active');
+        }
+
+        const img = document.createElement('img');
+        img.src = conteudo.urlImagemCapa;
+        img.alt = conteudo.titulo;
+        img.classList.add('d-block', 'w-100');
+
+        const carouselCaption = document.createElement('div');
+        carouselCaption.classList.add('carousel-caption', 'd-none', 'd-md-block');
+        carouselCaption.innerHTML = `
+          <h5>${conteudo.titulo}</h5>
+          <p>${conteudo.descricao}</p>
+        `;
+
+        carouselItem.appendChild(img);
+        carouselItem.appendChild(carouselCaption);
+        carouselInner.appendChild(carouselItem);
+      });
+    })
+    .catch(error => {
+      console.error('Erro ao buscar conteúdos sugeridos:', error);
+      
+    });
+
+  
+  const colegasDeTrabalhoUrl = `${apiUrl}/colegasDeTrabalho`;
+
+  fetch(colegasDeTrabalhoUrl)
+    .then(response => response.json())
+    .then(data => {
+      const colegasContainer = document.getElementById('colegasContainer');
+      if (colegasContainer) {
+        data.forEach(colega => {
+          const col = document.createElement('div');
+          col.classList.add('col-md-2');
+
+          const img = document.createElement('img');
+          img.src = colega.urlFoto;
+          img.alt = colega.nome;
+          img.classList.add('img-fluid');
+
+          const p = document.createElement('p');
+          p.classList.add('text-center');
+          const a = document.createElement('a');
+          a.href = colega.urlGitHub;
+          a.textContent = colega.nome;
+          a.target = '_blank'; 
+
+          p.appendChild(a);
+          col.appendChild(img);
+          col.appendChild(p);
+
+          colegasContainer.appendChild(col);
+        });
+      } else {
+        console.error('Elemento #colegasContainer não encontrado.');
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao buscar colegas de trabalho:', error);
+      
     });
 });
